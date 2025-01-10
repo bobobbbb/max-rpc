9aa9e07a2cf84605c87625f61e807d27aae8b74f
bobobbbb <1665052137@qq.com>
2025-01-10 15:19:21 +0800

    自定义协议（粘包半包为解决）完成，将上个版本的application.properties中的代码部分删除

--- example-provider/src/main/java/com/max/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/max/example/provider/ProviderExample.java
@@ -1,4 +1,5 @@
 package com.max.example.provider;
+
 import com.max.example.common.service.UserService;
 import com.max.maxrpc.RpcApplication;
 import com.max.maxrpc.config.RegistryConfig;
@@ -7,8 +8,7 @@ import com.max.maxrpc.model.ServiceMetaInfo;
 import com.max.maxrpc.registry.LocalRegistry;
 import com.max.maxrpc.registry.Registry;
 import com.max.maxrpc.registry.RegistryFactory;
-import com.max.maxrpc.server.HttpServer;
-import com.max.maxrpc.server.VertxHttpServer;
+import com.max.maxrpc.server.tcp.VertxTcpServer;
 
 /**
  * 服务提供者示例
@@ -22,28 +22,25 @@ public class ProviderExample {
     public static void main(String[] args) {
         // RPC 框架初始化
         RpcApplication.init();
-
         // 注册服务
         String serviceName = UserService.class.getName();
         LocalRegistry.register(serviceName, UserServiceImpl.class);
-
         // 注册服务到注册中心
         RpcConfig rpcConfig = RpcApplication.getRpcConfig();
         RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-        //在这里使用loadspi加载custom中的etcdregistry类，并在这里取到；
         Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
         ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
         serviceMetaInfo.setServiceName(serviceName);
         serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
         serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
         try {
-            //调用的是etcd的注册方法 将这个service的信息注册进去etcd；
             registry.register(serviceMetaInfo);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        // 启动 web 服务
-        HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+
+        // 启动 TCP 服务
+        VertxTcpServer vertxTcpServer = new VertxTcpServer();
+        vertxTcpServer.doStart(8080);
     }
 }
